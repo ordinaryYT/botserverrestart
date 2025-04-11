539ffb22b4082589dc2c233e1ae6af490fb111de
ordinaryYT <baileybailey698@gmail.com>
2025-04-11 11:34:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post("/restart", async (req, res) => {
     // Trigger the Render deploy hook
     const response = await axios.post(deployHookUrl, {}, {
       headers: {
-        'Authorization': `Bearer YOUR_RENDER_API_KEY`  // Add your Render API key here
+        'Authorization': `Bearer rnd_MDCB3izGs5qFQlDXtzMEgX29Iqmn`  // Add your Render API key here
       }
     });
 
